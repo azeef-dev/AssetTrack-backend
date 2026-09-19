cde6eed359dc7c607ff8194cb731e7f75a6f5ccd
azeef-dev <azeefashraf22@gmail.com>
2026-09-19 11:43:31 -0700

    fix: update default server port from 4000 to 5000

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     await connectDB();
